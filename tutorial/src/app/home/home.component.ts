18e69a37ab64c2d2ef845fe32c0ae731bb81a21e
Peter Le <ptl7263@rit.edu>
2024-05-14 19:30:43 -0400

    Built menu

--- tutorial/src/app/home/home.component.ts
+++ tutorial/src/app/home/home.component.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
-import { getLocaleExtraDayPeriodRules } from '@angular/common';
-import { Products } from '../types';
+import { CommonModule, getLocaleExtraDayPeriodRules } from '@angular/common';
+import { Products, Product } from '../types';
+import { ProductComponent } from '../components/product/product.component';
 
 @Component({
-  selector: 'app-home', 
+  selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductService) {}
 
+  products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product, "Output")
+  }
+
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
-      .subscribe((products : Products) => {
-        console.log(products.items);
+      .subscribe((products: Products) => {
+        this.products = products.items;
       });
   }
 }
