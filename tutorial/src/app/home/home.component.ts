c98eb5534b0e5079ea815f23659703f539ff0981
Peter Le <ptl7263@rit.edu>
2024-05-15 15:43:08 -0400

    Made all endpoints to this project

--- tutorial/src/app/home/home.component.ts
+++ tutorial/src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { ProductService } from '../services/product.service';
 import { CommonModule, getLocaleExtraDayPeriodRules } from '@angular/common';
 import { Products, Product } from '../types';
 import { ProductComponent } from '../components/product/product.component';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -16,15 +17,28 @@ export class HomeComponent {
 
   products: Product[] = [];
 
+  totalRecords: number = 0;
+
+  rows: number = 5;
+
   onProductOutput(product: Product) {
-    console.log(product, "Output")
+    console.log(product, 'Output');
+  }
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page, perPage })
+      .subscribe((products: Products) => {
+        this.products = products.items;
+        this.totalRecords = products.total;
+      });
   }
 
   ngOnInit() {
-    this.productsService
-      .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
-      .subscribe((products: Products) => {
-        this.products = products.items;
-      });
+    this.fetchProducts(0, this.rows);
   }
 }
