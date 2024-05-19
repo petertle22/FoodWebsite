a6cf94a80562f22c732897fa1b70941e74acf28a
Peter Le <ptl7263@rit.edu>
2024-05-19 19:49:29 -0400

    Finished following tutorial

--- tutorial/src/app/components/edit-popup/edit-popup.component.ts
+++ tutorial/src/app/components/edit-popup/edit-popup.component.ts
@@ -1,22 +1,33 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogModule } from 'primeng/dialog';
-import { Product } from '../../types'; 
-import { FormsModule } from '@angular/forms';
+import { Product } from '../../types';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
-
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [DialogModule, CommonModule, FormsModule, RatingModule, ButtonModule],
+  imports: [
+    DialogModule,
+    CommonModule,
+    FormsModule,
+    RatingModule,
+    ButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './edit-popup.component.html',
-  styleUrl: './edit-popup.component.scss'
+  styleUrl: './edit-popup.component.scss',
 })
 export class EditPopupComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
   @Input() display: boolean = false;
-  @Output() displayChange  = new EventEmitter<boolean>();
+  @Output() displayChange = new EventEmitter<boolean>();
   @Output() confirm = new EventEmitter<any>();
   @Input() header!: string;
 
@@ -27,8 +38,29 @@ export class EditPopupComponent {
     rating: 0,
   };
 
+  specialCharacterValidator(): ValidatorFn {
+    return (control) => {
+      const hasSpecialCharacter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(
+        control.value
+      );
+
+      return hasSpecialCharacter ? { hasSpecialCharacter: true } : null;
+    };
+  }
+
+  productForm = this.formBuilder.group({
+    name: ['', [Validators.required, this.specialCharacterValidator()]],
+    image: [''],
+    price: ['', Validators.required],
+    rating: [0],
+  });
+
+  ngOnChanges() {
+    this.productForm.patchValue(this.product);
+  }
+
   onConfirm() {
-    this.confirm.emit(this.product);
+    this.confirm.emit(this.productForm.value);
     this.display = false;
     this.displayChange.emit(this.display);
   }
--- tutorial/src/app/components/product/product.component.ts
+++ tutorial/src/app/components/product/product.component.ts
@@ -1,22 +1,37 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Product } from '../../types';
-import { RatingModule } from 'primeng/rating'
+import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { ConfirmationService } from 'primeng/api';
-import { ConfirmPopupModule } from 'primeng/confirmpopup'
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { ToastModule } from 'primeng/toast';
+import { PricePipe } from '../../pipes/price.pipe';
+import { TruncateNamePipe } from '../../pipes/truncate-name.pipe';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule, ButtonModule, ConfirmPopupModule, ToastModule],
+  imports: [
+    RatingModule,
+    FormsModule,
+    ButtonModule,
+    ConfirmPopupModule,
+    ToastModule,
+    PricePipe,
+    TruncateNamePipe,
+  ],
   providers: [ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
 export class ProductComponent {
-
   constructor(private confirmationService: ConfirmationService) {}
 
   @ViewChild('deleteButton') deleteButton: any;
@@ -43,7 +58,5 @@ export class ProductComponent {
     this.delete.emit(this.product);
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 }
